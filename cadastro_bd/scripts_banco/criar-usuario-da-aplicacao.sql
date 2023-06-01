d3406a4084d414c3b8e2ef7a50310e9829d762cc
marcio.araya <marcio.araya@gmail.com>
2023-06-01 09:43:31 -0300

    corrige nome do schema

--- cadastro_bd/scripts_banco/criar-usuario-da-aplicacao.sql
+++ cadastro_bd/scripts_banco/criar-usuario-da-aplicacao.sql
@@ -10,4 +10,4 @@ ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'senac';
 
 /* criar usuário específico para aplicação agenda */
 CREATE USER 'aplicacao_curso'@'localhost' IDENTIFIED WITH mysql_native_password BY 'curso123'; 
-grant select, insert, update, delete on agenda.* to 'aplicacao_curso'@'localhost' ;
+grant select, insert, update, delete on curso.* to 'aplicacao_curso'@'localhost' ;
